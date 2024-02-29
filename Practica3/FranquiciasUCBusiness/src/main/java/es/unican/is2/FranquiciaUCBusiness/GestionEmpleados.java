c4d4ecddd02fbcf71f3f27527110fcc35d81b8d4
scg194 <scg194@alumnos.unican.es>
2024-02-29 13:30:03 +0100

    Añadidas clases

---
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/FranquiciaUCBusiness/GestionEmpleados.java
@@ -0,0 +1,25 @@
+package es.unican.is2.FranquiciaUCBusiness;
+
+public class GestionEmpleados {
+	
+	private NuevoEmpleado() {
+		
+	}
+	
+	private DarBajaEmpleado() {
+		
+	}
+	
+	private MoverEmpleado() {
+		
+	}
+	
+	private RegistrarAltaMedica() {
+		
+	}
+	
+	private RegistrarBajaMedica() {
+		
+	}	
+
+}
---
+++ Practica3/FranquiciasUCBusiness/src/main/java/es/unican/is2/FranquiciaUCBusiness/GestionTiendas.java
@@ -0,0 +1,5 @@
+package es.unican.is2.FranquiciaUCBusiness;
+
+public class GestionTiendas implements IGestionTiendas{
+	
+}
